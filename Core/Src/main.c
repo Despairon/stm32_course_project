c9d01c1d7e671588f3ba638abeb29c41461c7e77
Despairon <maxdespairon@gmail.com>
2025-04-23 01:18:14 +0300

    Initial commit

---
+++ Core/Src/main.c
@@ -0,0 +1,578 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdbool.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+typedef enum pwm_led_mode_e
+{
+  PWM_LED_MODE_1X = 1,
+  PWM_LED_MODE_2X,
+  PWM_LED_MODE_3X,
+  PWM_LED_MODE_LAST
+} pwm_led_mode_t;
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+#define MAX7219_DATA_SIZE (2U)
+#define LSM6DS3_I2C_ADDRESS ((uint16_t)0b1101010 << 1)
+#define LSM6DS3_WHO_AM_I_REG_ADDR (0x0f)
+#define LSM6DS3_WHO_AM_I_REG_DEFAULT_VAL (0b01101001)
+#define LSM6DS3_OUTX_L_G_REG_ADDR (0x22)
+#define LSM6DS3_OUTX_H_G_REG_ADDR (0x23)
+#define LSM6DS3_CTRL2_G_REG_ADDR (0x11)
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
+SPI_HandleTypeDef hspi1;
+
+TIM_HandleTypeDef htim1;
+
+UART_HandleTypeDef huart6;
+DMA_HandleTypeDef hdma_usart6_tx;
+
+/* USER CODE BEGIN PV */
+
+static pwm_led_mode_t pwm_led_mode = PWM_LED_MODE_1X;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
+static void MX_TIM1_Init(void);
+static void MX_SPI1_Init(void);
+static void MX_I2C1_Init(void);
+static void MX_USART6_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  UNUSED(htim);
+
+  TIM1->CCR1 = TIM1->CCR1 < TIM1->ARR ? TIM1->CCR1 + (uint32_t)pwm_led_mode : 0;
+}
+
+static void uart_rx_complete(UART_HandleTypeDef *huart)
+{
+  const uint8_t data_tx[] = "Received 32 bytes!\r\n";
+  HAL_UART_Transmit(&huart6, data_tx, (uint16_t)sizeof(data_tx), 100);
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+  
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_TIM1_Init();
+  MX_SPI1_Init();
+  MX_I2C1_Init();
+  MX_USART6_UART_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_TIM_Base_Start_IT(&htim1);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+  TIM1->CCR1 = 0;
+
+  static bool button_pressed = false;
+
+  const uint8_t spi_data_to_send_list[][MAX7219_DATA_SIZE] =
+  {
+    {0x0c, 0x01}, // Set shutdown mode to normal operation
+    {0x09, 0x00}, // Set decode mode to no decode
+    {0x0a, 0x03}, // Set brightness to 3
+    {0x0b, 0x07}, // Set scan limit to scan all digits
+    {0x0f, 0x00}, // Disable test mode
+    {0x01, 0x00}, // clear row 1
+    {0x02, 0x00}, // clear row 2
+    {0x03, 0x00}, // clear row 3
+    {0x04, 0x00}, // clear row 4
+    {0x05, 0x00}, // clear row 5
+    {0x06, 0x00}, // clear row 6
+    {0x07, 0x00}, // clear row 7
+    {0x08, 0x00}, // clear row 8
+    {0x01, 0x07}, // light up first three leds (segments A, B, C) of row 0 (digit 0)
+    {0x02, 0x07}, // light up first three leds (segments A, B, C) of row 1 (digit 1)
+    {0x03, 0x07}, // light up first three leds (segments A, B, C) of row 2 (digit 2)
+    {0x04, 0x07}, // light up first three leds (segments A, B, C) of row 3 (digit 3)
+    {0x05, 0x07}, // light up first three leds (segments A, B, C) of row 4 (digit 4)
+    {0x06, 0x07}, // light up first three leds (segments A, B, C) of row 5 (digit 5)
+    {0x07, 0x07}, // light up first three leds (segments A, B, C) of row 6 (digit 6)
+    {0x08, 0x07}  // light up first three leds (segments A, B, C) of row 6 (digit 7)
+  };
+  const size_t spi_data_to_send_list_size = sizeof(spi_data_to_send_list) / sizeof(spi_data_to_send_list[0]);
+
+  // HAL_UART_RegisterCallback(&huart6, HAL_UART_RX_COMPLETE_CB_ID, &uart_rx_complete);
+
+  // uint8_t data_rx[32] = {0};
+  // HAL_UART_Receive_IT(&huart6, data_rx, (uint16_t)sizeof(data_rx));
+
+  // const uint8_t data_tx[] = "Hello world!\r\n";
+  // HAL_UART_Transmit_DMA(&huart6, data_tx, (uint16_t)sizeof(data_tx));
+  // TODO: Get accelerometer/gyro data from the module via IT or DMA
+  // TODO: Send accelerometer/gyro data to UART
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+    // if ((GPIOA->IDR & 0x1) == GPIO_PIN_RESET)
+    // {
+    //   if (!button_pressed)
+    //   {
+    //     pwm_led_mode++;
+
+    //     if (pwm_led_mode == PWM_LED_MODE_LAST)
+    //       pwm_led_mode = PWM_LED_MODE_1X;
+
+    //     button_pressed = true;
+    //     HAL_Delay(20);
+    //   }
+    // }
+    // else
+    // {
+    //   if (button_pressed)
+    //   {
+    //     button_pressed = false;
+    //     HAL_Delay(20);
+    //   }
+    // }
+
+    // const uint8_t *data = spi_data_to_send_list[0];
+    // do
+    // {
+    //   HAL_GPIO_WritePin(GPIOA, 4, GPIO_PIN_RESET);
+    //   HAL_Delay(10);
+    //   HAL_StatusTypeDef result = HAL_SPI_Transmit(&hspi1, (const uint8_t*)data, (uint16_t)MAX7219_DATA_SIZE, 1000);
+    //   if (result != HAL_OK)
+    //   {
+    //     // TODO: error handling
+    //   }
+    //   HAL_Delay(10);
+    //   HAL_GPIO_WritePin(GPIOA, 4, GPIO_PIN_SET);
+    //   data += sizeof(spi_data_to_send_list[0]);
+    // }
+    // while (data != spi_data_to_send_list[spi_data_to_send_list_size]);
+
+    HAL_StatusTypeDef result = HAL_I2C_IsDeviceReady(&hi2c1, LSM6DS3_I2C_ADDRESS, 10, 1000);
+    if (result != HAL_OK)
+    {
+      // TODO: error handling
+      continue;
+    }
+
+    uint8_t whoami_reg_val = 0;
+    result = HAL_I2C_Mem_Read(&hi2c1, LSM6DS3_I2C_ADDRESS, LSM6DS3_WHO_AM_I_REG_ADDR, 1, &whoami_reg_val, 1, 1000);
+    if ((result != HAL_OK) || (whoami_reg_val != LSM6DS3_WHO_AM_I_REG_DEFAULT_VAL))
+    {
+      // TODO: error handling
+      continue;
+    }
+
+    const uint8_t ctrl2_g_reg_val = 0x30;
+    result = HAL_I2C_Mem_Write(&hi2c1, LSM6DS3_I2C_ADDRESS, LSM6DS3_CTRL2_G_REG_ADDR, 1, &ctrl2_g_reg_val, 1, 1000);
+    if (result != HAL_OK)
+    {
+      // TODO: error handling
+      continue;
+    }
+
+    uint16_t outx_gyro = 0;
+    result = HAL_I2C_Mem_Read(&hi2c1, LSM6DS3_I2C_ADDRESS, LSM6DS3_OUTX_H_G_REG_ADDR, 1, &outx_gyro, 1, 1000);
+    if (result != HAL_OK)
+    {
+      // TODO: error handling
+      continue;
+    }
+
+    outx_gyro <<= 8;
+
+    result = HAL_I2C_Mem_Read(&hi2c1, LSM6DS3_I2C_ADDRESS, LSM6DS3_OUTX_L_G_REG_ADDR, 1, &outx_gyro, 1, 1000);
+    if (result != HAL_OK)
+    {
+      // TODO: error handling
+      continue;
+    }
+
+    HAL_Delay(200);
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 20000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void)
+{
+
+  /* USER CODE BEGIN SPI1_Init 0 */
+
+  /* USER CODE END SPI1_Init 0 */
+
+  /* USER CODE BEGIN SPI1_Init 1 */
+
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_1LINE;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_HIGH;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_128;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
+
+  /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 26-1;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 1000-1;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+
+}
+
+/**
+  * @brief USART6 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART6_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART6_Init 0 */
+
+  /* USER CODE END USART6_Init 0 */
+
+  /* USER CODE BEGIN USART6_Init 1 */
+
+  /* USER CODE END USART6_Init 1 */
+  huart6.Instance = USART6;
+  huart6.Init.BaudRate = 38400;
+  huart6.Init.WordLength = UART_WORDLENGTH_8B;
+  huart6.Init.StopBits = UART_STOPBITS_1;
+  huart6.Init.Parity = UART_PARITY_NONE;
+  huart6.Init.Mode = UART_MODE_TX_RX;
+  huart6.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart6.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart6) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART6_Init 2 */
+
+  /* USER CODE END USART6_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA2_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA2_Stream6_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA2_Stream6_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Stream6_IRQn);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
+
+  /*Configure GPIO pin : PA0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
